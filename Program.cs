f0b7df5839307858da0b8f0b5c8ec00fa8f95107
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 02:12:12 +0500

    Add, Remove and Serach Functionalties

--- Program.cs
+++ Program.cs
@@ -8,8 +8,35 @@ namespace _7._6_Working_with_Lists__Add__Remove__Search_
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Console.WriteLine("List of Person Names:");
+            List<string> Person = new List<string>();
+            Person.Add("Abdul Hannan");
+            Person.Add("Abdul Rehman");
+            Person.Add("Muhammad SAW");
+            Person.Add("Mueen");
+            Person.Add("Ghanam");
+            foreach (var item in Person) 
+            {
+                Console.WriteLine(item);
+            }
+            Person.Remove("Mueen");
+            Console.WriteLine("\nList after Remove 'Mueen' :");
+            foreach (var item in Person)
+            {
+                Console.WriteLine(item);
+            }
+            if (Person.Contains("Muhammad SAW"))
+            {
+
+                Console.WriteLine("\n'Muhammad SAW' Found in the List.");
+            }
+            else 
+            {
+                Console.WriteLine("\n'Muhammad SAW' Found in the List.");
+            }
         }
     }
 }
