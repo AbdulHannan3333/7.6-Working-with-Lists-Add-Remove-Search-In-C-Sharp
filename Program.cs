bfb126771a7d4e689519df1a825edba8d707c8d5
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 01:59:27 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._6_Working_with_Lists__Add__Remove__Search_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
